9f0fb6094a94458a1e2d473ea0c42e41dfdd8594
DSOrnstein <namphonggithub@gmail.com>
2025-12-11 22:55:57 +0700

    feat(be): add deck methods to api server

--- backend/internal/api/decks.go
+++ backend/internal/api/decks.go
@@ -1,9 +1,19 @@
 package api
 
 import (
+	"main/internal/generated/openapi"
+
 	"github.com/gin-gonic/gin"
 )
 
-func (apiServer *ApiServer) GetApiDecks(ctx *gin.Context) {
-	apiServer.Decks.GetApiDecks(ctx)
+func (apiServer *ApiServer) GetDecks(ctx *gin.Context, params openapi.GetDecksParams) {
+	apiServer.Decks.GetDecks(ctx, params)
+}
+
+func (apiServer *ApiServer) GetDeckById(ctx *gin.Context, id string) {
+	apiServer.Decks.GetDeckById(ctx, id)
+}
+
+func (apiServer *ApiServer) PostDeck(ctx *gin.Context) {
+	apiServer.Decks.PostDeck(ctx)
 }
